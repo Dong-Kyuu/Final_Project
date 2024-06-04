170cf9eb165b8ba3e1c4ec1a391a54a8de12d2a5
동규 <iopz8811@gmail.com>
2024-06-04 16:03:21 +0900

    ADD 그

--- src/main/java/com/example/jhta_3team_finalproject/controller/CommentController.java
+++ src/main/java/com/example/jhta_3team_finalproject/controller/CommentController.java
@@ -2,12 +2,15 @@ package com.example.jhta_3team_finalproject.controller;
 
 import com.example.jhta_3team_finalproject.domain.Table.Comment;
 import com.example.jhta_3team_finalproject.service.table.TableCommentService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.HashMap;
 import java.util.List;
@@ -45,6 +48,22 @@ public class CommentController {
         logger.info(co.toString());
         logger.info("등록실행!");
         return CS.commentsInsert(co);
+    }
+
+    @PostMapping("/delete")
+    public Map<String, Object>  DeleteAction(int bnum, int cnum) {
+        Map<String, Object> result = new HashMap<>();
+        int deleteresult = CS.commentsDelete(cnum);
+
+        if (deleteresult == 0) {
+            result.put("status", "error");
+            result.put("message", "삭제 실패");
+        } else {
+            result.put("status", "success");
+            result.put("redirect", "/table/detail?num=" + bnum);
+        }
+
+        return result;
     }
 
 }
