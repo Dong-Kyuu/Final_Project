2d6ecee383032c8dbb6b91e7031df9a955fe6cd6
zihun <zilun@naver.com>
2024-06-06 06:23:35 +0900

    Design: 다이얼 로그 오픈 위치 변경, 화면 크기 조절 기능 추가, 아이콘-배경색 변경

--- src/main/java/com/example/jhta_3team_finalproject/controller/ChatController.java
+++ src/main/java/com/example/jhta_3team_finalproject/controller/ChatController.java
@@ -24,6 +24,11 @@ public class ChatController {
     List<ChatRoom> chatRoomList;
     static int roomNumber = 0;
 
+    @RequestMapping(value = "scale")
+    public String testscale() {
+        return "chat/testscale";
+    }
+
     @RequestMapping(value = "chatview")
     public String chatView() {
         return "chat/blank-page";
