d3899cc0375f935a6ffe95f0a6ea19baba8fe48b
kim san <rlatks15@naver.com>
2024-06-12 17:10:32 +0900

    Add: trip Register S3이용한 추가

--- src/main/java/com/example/jhta_3team_finalproject/domain/TourPackage/Trip.java
+++ src/main/java/com/example/jhta_3team_finalproject/domain/TourPackage/Trip.java
@@ -4,96 +4,29 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@ToString
+@Setter
+@Getter
 public class Trip {
-    String tripNo;
+    private int tripNo;
+    private String tripName;
+    private int tripPrice;
+    private int tripStock;
+    private int tripMaxStock;
+    private String regDate;
+    private String expireDate;
+    private String tripDate;
+    private String fileId;
+    private String tripMainImg;
+    private String tripCategory;
+    private String optionIds;
 
-    @NotBlank(message = "상품 이름을 입력하세요.")
-    String tripName;
-
-    @NotNull(message = "상품 가격을 입력하세요.")
-    @Min(value = 0, message = "상품 가격은 0 이상이어야 합니다.")
-    int tripPrice;
-
-    int tripStock;
-    int tripMaxStock;
-
-    String regDate;
-    String expireDate;
-    String tripDate;
-    String fileId;
-    String tripMainIMG;
-    String tripCategory;
-    String optionIds;
-
-
-
-    public String getTripNo() {
-        return tripNo;
-    }
-    public void setTripNo(String tripNo) {
-        this.tripNo = tripNo;
-    }
-    public String getTripName() {
-        return tripName;
-    }
-    public void setTripName(String tripName) {
-        this.tripName = tripName;
-    }
-    public int getTripPrice() {
-        return tripPrice;
-    }
-    public void setTripPrice(int tripPrice) {
-        this.tripPrice = tripPrice;
-    }
-    public int getTripStock() {
-        return tripStock;
-    }
-    public void setTripStock(int tripStock) {
-        this.tripStock = tripStock;
-    }
-    public int getTripMaxStock() {
-        return tripMaxStock;
-    }
-    public void setTripMaxStock(int tripMaxStock) {
-        this.tripMaxStock = tripMaxStock;
-    }
-    public String getRegDate() {
-        return regDate;
-    }
-    public void setRegDate(String regDate) {
-        this.regDate = regDate;
-    }
-    public String getExpireDate() {
-        return expireDate;
-    }
-    public void setExpireDate(String expireDate) {
-        this.expireDate = expireDate;
-    }
-    public String getTripDate() {
-        return tripDate;
-    }
-    public void setTripDate(String tripDate) {
-        this.tripDate = tripDate;
-    }
-    public String getFileNo() {
-        return fileId;
-    }
-    public void setFileNo(String fileId) {
-        this.fileId = fileId;
-    }
-    public String getTripMainIMG() {
-        return tripMainIMG;
-    }
-    public void setTripMainIMG(String tripMainIMG) {
-        this.tripMainIMG = tripMainIMG;
-    }
-    public String getTripCategory() {
-        return tripCategory;
-    }
-    public void setTripCategory(String tripCategory) {
-        this.tripCategory = tripCategory;
-    }
-    public String getOptionIds() {return optionIds;}
-    public void setOptionIds(String optionIds) {this.optionIds = optionIds;}
 }
