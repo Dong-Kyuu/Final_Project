5b9831f81694e4357e5305dcd7eb5b4e99d25e47
DESKTOP-SO3G7VU\jhta <a66074742@gmail.com>
2024-06-12 12:23:23 +0900

    Fix: 채팅 UI 버그 전체 해결 1. 채팅 시 화면이 이상하게 보이던 문제 해결 2. 채팅방이 보여지는 순서를 조정 (안 읽은 메시지와 채팅방이 생성된 시간 중 큰 값으로 보여줌)

--- src/main/java/com/example/jhta_3team_finalproject/config/AwsConfig.java
+++ src/main/java/com/example/jhta_3team_finalproject/config/AwsConfig.java
@@ -1,4 +1,4 @@
-package com.example.jhta_3team_finalproject.config.chat;
+package com.example.jhta_3team_finalproject.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
