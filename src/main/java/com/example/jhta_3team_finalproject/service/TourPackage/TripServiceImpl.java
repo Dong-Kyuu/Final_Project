b8e4c69228527f731dc4ebbafc36e297a5e3d432
kim san <rlatks15@naver.com>
2024-06-14 17:48:20 +0900

    ADD: trip purchase테이블 등록 - 결제시 예약등록 / trip register 상품 상태 변경 가능

--- src/main/java/com/example/jhta_3team_finalproject/service/TourPackage/TripServiceImpl.java
+++ src/main/java/com/example/jhta_3team_finalproject/service/TourPackage/TripServiceImpl.java
@@ -1,11 +1,16 @@
 package com.example.jhta_3team_finalproject.service.TourPackage;
 import com.example.jhta_3team_finalproject.domain.TourPackage.*;
+import com.example.jhta_3team_finalproject.domain.User.User;
 import com.example.jhta_3team_finalproject.mybatis.mapper.TourPackage.TripMapper;
+import com.example.jhta_3team_finalproject.service.Notification.SseService;
 import com.example.jhta_3team_finalproject.service.S3.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import com.example.jhta_3team_finalproject.service.Notification.SseService;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -18,14 +23,16 @@ public class TripServiceImpl implements TripService{
     private final S3Service s3Service;
     private final TripMapper tripMapper;
     private final RedisTemplate<String, Object> redisTemplate;
+    private final SseService sseService;
 
     private static final String STOCK_PREFIX = "trip:stock:";
 
     @Autowired
-    public TripServiceImpl(S3Service s3Service, TripMapper tripMapper, RedisTemplate<String, Object> redisTemplate) {
+    public TripServiceImpl(S3Service s3Service, TripMapper tripMapper, RedisTemplate<String, Object> redisTemplate, SseService sseService) {
         this.s3Service = s3Service;
         this.tripMapper = tripMapper;
         this.redisTemplate = redisTemplate;
+        this.sseService = sseService;
     }
 
     @Override
@@ -43,6 +50,20 @@ public class TripServiceImpl implements TripService{
         return tripMapper.getDetail(num);
     }
 
+
+    //-------------------
+    //이렇게 3개 묶을만 함 + approved만 보여줘야함
+
+    public List<Trip> getTripList2(int startRow, int endRow, String sort, String classification, String condition){
+        if(classification.equals("category")){
+            return tripMapper.getCategoryTripList(startRow, endRow, condition, sort);
+        }else if(classification.equals("keyword")){
+            return tripMapper.getTripListByKeyword(startRow, endRow, condition,sort);
+        }else{
+            return tripMapper.getTripList(startRow, endRow, sort);
+        }
+    }
+
     @Override
     public List<Trip> getTripList(int startRow, int endRow, String sort) {
         return tripMapper.getTripList(startRow, endRow, sort);
@@ -57,6 +78,7 @@ public class TripServiceImpl implements TripService{
     public List<Trip> getTripListByKeyword(int startRow, int endRow, String keyword, String sort) {
         return tripMapper.getTripListByKeyword(startRow, endRow, keyword,sort);
     }
+    //-------------------
 
     @Override
     public List<Trip> getAllTrip() {
@@ -101,11 +123,22 @@ public class TripServiceImpl implements TripService{
                 trip.getTripMaxStock(), trip.getTripDate(),String.valueOf(currentDate), trip.getExpireDate(),
                 mainIMG, trip.getTripCategory(), trip.getOptionIds(), fileId);
 
+        // SSE 알림 보내기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User loginUser = (User) authentication.getPrincipal();
+        int userNum = loginUser.getUserNum();
+        System.out.println("userNum = "+userNum);
+        String message = "상품 : " + trip.getTripName() + "이 결제 대기 상태입니다.";
+       // sseService.sendNotification(userNum, message);
+
         // 재고를 Redis에 저장
+        /*
         int tripNo = trip.getTripNo();
         Integer tripStock = trip.getTripStock(); // int를 Integer로 오토박싱
         redisTemplate.opsForValue().set(STOCK_PREFIX + tripNo, tripStock);
+         */
     }
+
 /*
     //@Override
     public int getTripStock(String tripNo) {
@@ -118,4 +151,23 @@ public class TripServiceImpl implements TripService{
         redisTemplate.opsForValue().set(STOCK_PREFIX + tripNo, tripStock);
     }
 */
+
+
+    @Override
+    public List<Trip> getApprovedTrip() {
+        String status = "APPROVED";
+        return tripMapper.getTripByStatus(status);
+    }
+
+    @Override
+    public List<Trip> getPendingTrip() {
+        String status = "PENDING";
+        return tripMapper.getTripByStatus(status);
+    }
+
+    @Override
+    public void updateTripStatus(int tripNo, String status) {
+        tripMapper.updateTripStatus(tripNo,status);
+    }
+
 }
