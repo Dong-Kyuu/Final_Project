0bfbcc605aaa0c1cf4e7aa78c80c5df5306f4684
DESKTOP-SO3G7VU\jhta <a66074742@gmail.com>
2024-06-15 15:18:57 +0900

    Refactor: tailwind cdn -> css 변경

---
+++ src/main/resources/static/css/chat/tailwind.config.js
@@ -0,0 +1,11 @@
+/** npx tailwindcss -i ./tailwind.config.css -o ./tailwind.css */
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../../../**/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: []
+}
+
